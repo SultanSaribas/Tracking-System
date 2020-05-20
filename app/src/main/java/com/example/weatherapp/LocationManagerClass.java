71799078510aa50e5e142587f0195bf36514a76e
Mustafa Kemal Özdemir <MustafaKemalOzdemir@users.noreply.github.com>
2020-05-20 15:22:06 +0300

    location enabled

---
+++ app/src/main/java/com/example/weatherapp/LocationManagerClass.java
@@ -0,0 +1,74 @@
+package com.example.weatherapp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class LocationManagerClass {
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference user = database.getReference("users");
+    LocationListener locationListener;
+    LocationManager locationManager;
+
+    private Context mContext;
+    private String userId;
+
+    public LocationManagerClass(Context context) {
+        this.mContext = context;
+    }
+
+    @SuppressLint({"MissingPermission", "HardwareIds", "ServiceCast"})
+    public void start() {
+        Log.v("locationUpdate", "started");
+        userId = Settings.Secure.getString(mContext.getContentResolver(), Settings.Secure.ANDROID_ID);
+        String key =user.getKey();
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.v("locationUpdate", "updated");
+                try{
+                    user.child(userId).child("latitude").setValue(location.getLatitude());
+                    user.child(userId).child("longitude").setValue(location.getLongitude());
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager != null) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            Log.v("locationUpdate", "triggered");
+        }else{
+            Log.v("locationUpdate", "null porblem");
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    LocationManagerClass locationManagerClass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationManagerClass = new LocationManagerClass(this);
+        boolean permissionStatus = false;
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED || checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) !=  PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.READ_PHONE_STATE}, PackageManager.PERMISSION_GRANTED);
+            permissionStatus = true;
+        }
+        startService(new Intent(getApplicationContext(),LocationTrackerService.class));
+        locationManagerClass.start();
+
+
     }
 }
